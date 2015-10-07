3f2ab506d6f6b9cf152c69ddb20a2b5ddeaa08da
eunsebi <eunsebi@me.com>
2015-10-07 19:25:04 +0900

    form edit
    Signed-off-by: eunsebi <eunsebi@me.com>

--- BaKasMacro/BaKasMacro/MainForm.Designer.cs
+++ BaKasMacro/BaKasMacro/MainForm.Designer.cs
@@ -19,6 +19,27 @@ namespace BaKasMacro
 		private System.Windows.Forms.TabPage tabPage2;
 		private System.Windows.Forms.TabPage tabPage3;
 		private System.Windows.Forms.TabPage tabPage4;
+		private System.Windows.Forms.Button button5;
+		private System.Windows.Forms.Button button4;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.ListBox listBox2;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.ListBox listBox1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TabControl tabControl2;
+		private System.Windows.Forms.TabPage tabPage5;
+		private System.Windows.Forms.ListBox listBox3;
+		private System.Windows.Forms.TabPage tabPage6;
+		private System.Windows.Forms.TabPage tabPage7;
+		private System.Windows.Forms.TabPage tabPage8;
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.Panel panel2;
+		private System.Windows.Forms.Panel panel3;
+		private System.Windows.Forms.Panel panel4;
 		
 		/// <summary>
 		/// Disposes resources used by the form.
@@ -39,14 +60,40 @@ namespace BaKasMacro
 		/// Do not change the method contents inside the source code editor. The Forms designer might
 		/// not be able to load this method if it was changed manually.
 		/// </summary>
-		private void InitializeComponent()
-		{
+		private void InitializeComponent() {
 			this.tabControl1 = new System.Windows.Forms.TabControl();
 			this.tabPage1 = new System.Windows.Forms.TabPage();
+			this.button5 = new System.Windows.Forms.Button();
+			this.button4 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button1 = new System.Windows.Forms.Button();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.label3 = new System.Windows.Forms.Label();
+			this.listBox2 = new System.Windows.Forms.ListBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.listBox1 = new System.Windows.Forms.ListBox();
+			this.label1 = new System.Windows.Forms.Label();
 			this.tabPage2 = new System.Windows.Forms.TabPage();
+			this.tabControl2 = new System.Windows.Forms.TabControl();
+			this.tabPage5 = new System.Windows.Forms.TabPage();
+			this.listBox3 = new System.Windows.Forms.ListBox();
+			this.tabPage6 = new System.Windows.Forms.TabPage();
+			this.tabPage7 = new System.Windows.Forms.TabPage();
+			this.tabPage8 = new System.Windows.Forms.TabPage();
+			this.panel1 = new System.Windows.Forms.Panel();
 			this.tabPage3 = new System.Windows.Forms.TabPage();
 			this.tabPage4 = new System.Windows.Forms.TabPage();
+			this.panel2 = new System.Windows.Forms.Panel();
+			this.panel3 = new System.Windows.Forms.Panel();
+			this.panel4 = new System.Windows.Forms.Panel();
 			this.tabControl1.SuspendLayout();
+			this.tabPage1.SuspendLayout();
+			this.tabPage2.SuspendLayout();
+			this.tabControl2.SuspendLayout();
+			this.tabPage5.SuspendLayout();
+			this.tabPage6.SuspendLayout();
+			this.panel1.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// tabControl1
@@ -58,34 +105,228 @@ namespace BaKasMacro
 			this.tabControl1.Location = new System.Drawing.Point(6, 0);
 			this.tabControl1.Name = "tabControl1";
 			this.tabControl1.SelectedIndex = 0;
-			this.tabControl1.Size = new System.Drawing.Size(972, 272);
+			this.tabControl1.Size = new System.Drawing.Size(972, 285);
 			this.tabControl1.TabIndex = 0;
 			// 
 			// tabPage1
 			// 
+			this.tabPage1.BackColor = System.Drawing.Color.Black;
+			this.tabPage1.Controls.Add(this.button5);
+			this.tabPage1.Controls.Add(this.button4);
+			this.tabPage1.Controls.Add(this.button3);
+			this.tabPage1.Controls.Add(this.button2);
+			this.tabPage1.Controls.Add(this.button1);
+			this.tabPage1.Controls.Add(this.textBox1);
+			this.tabPage1.Controls.Add(this.label3);
+			this.tabPage1.Controls.Add(this.listBox2);
+			this.tabPage1.Controls.Add(this.label2);
+			this.tabPage1.Controls.Add(this.listBox1);
+			this.tabPage1.Controls.Add(this.label1);
 			this.tabPage1.Location = new System.Drawing.Point(4, 22);
 			this.tabPage1.Name = "tabPage1";
 			this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-			this.tabPage1.Size = new System.Drawing.Size(964, 246);
+			this.tabPage1.Size = new System.Drawing.Size(964, 259);
 			this.tabPage1.TabIndex = 0;
 			this.tabPage1.Text = "디바이스 선택";
-			this.tabPage1.UseVisualStyleBackColor = true;
+			// 
+			// button5
+			// 
+			this.button5.Location = new System.Drawing.Point(870, 194);
+			this.button5.Name = "button5";
+			this.button5.Size = new System.Drawing.Size(90, 31);
+			this.button5.TabIndex = 10;
+			this.button5.Text = "선 택";
+			this.button5.UseVisualStyleBackColor = true;
+			// 
+			// button4
+			// 
+			this.button4.Location = new System.Drawing.Point(765, 194);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(90, 31);
+			this.button4.TabIndex = 9;
+			this.button4.Text = "연 결";
+			this.button4.UseVisualStyleBackColor = true;
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(660, 194);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(90, 31);
+			this.button3.TabIndex = 8;
+			this.button3.Text = "검 색";
+			this.button3.UseVisualStyleBackColor = true;
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(511, 194);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(143, 31);
+			this.button2.TabIndex = 7;
+			this.button2.Text = "선 택";
+			this.button2.UseVisualStyleBackColor = true;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(368, 194);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(143, 31);
+			this.button1.TabIndex = 6;
+			this.button1.Text = "검 색";
+			this.button1.UseVisualStyleBackColor = true;
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(9, 229);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(646, 21);
+			this.textBox1.TabIndex = 5;
+			// 
+			// label3
+			// 
+			this.label3.ForeColor = System.Drawing.Color.White;
+			this.label3.Location = new System.Drawing.Point(6, 206);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(100, 20);
+			this.label3.TabIndex = 4;
+			this.label3.Text = "선택된 디바이스";
+			this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+			// 
+			// listBox2
+			// 
+			this.listBox2.FormattingEnabled = true;
+			this.listBox2.ItemHeight = 12;
+			this.listBox2.Location = new System.Drawing.Point(660, 24);
+			this.listBox2.Name = "listBox2";
+			this.listBox2.Size = new System.Drawing.Size(300, 160);
+			this.listBox2.TabIndex = 3;
+			// 
+			// label2
+			// 
+			this.label2.ForeColor = System.Drawing.Color.White;
+			this.label2.Location = new System.Drawing.Point(661, 3);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(100, 20);
+			this.label2.TabIndex = 2;
+			this.label2.Text = "원드/블택 목록";
+			this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+			// 
+			// listBox1
+			// 
+			this.listBox1.FormattingEnabled = true;
+			this.listBox1.ItemHeight = 12;
+			this.listBox1.Location = new System.Drawing.Point(5, 24);
+			this.listBox1.Name = "listBox1";
+			this.listBox1.Size = new System.Drawing.Size(650, 160);
+			this.listBox1.TabIndex = 1;
+			// 
+			// label1
+			// 
+			this.label1.ForeColor = System.Drawing.Color.White;
+			this.label1.Location = new System.Drawing.Point(6, 3);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(100, 20);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "디바이스 목록";
+			this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
 			// 
 			// tabPage2
 			// 
+			this.tabPage2.Controls.Add(this.tabControl2);
+			this.tabPage2.Controls.Add(this.panel1);
 			this.tabPage2.Location = new System.Drawing.Point(4, 22);
 			this.tabPage2.Name = "tabPage2";
 			this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-			this.tabPage2.Size = new System.Drawing.Size(469, 230);
+			this.tabPage2.Size = new System.Drawing.Size(964, 259);
 			this.tabPage2.TabIndex = 1;
 			this.tabPage2.Text = "게임 설정";
 			this.tabPage2.UseVisualStyleBackColor = true;
+			// 
+			// tabControl2
+			// 
+			this.tabControl2.Controls.Add(this.tabPage5);
+			this.tabControl2.Controls.Add(this.tabPage6);
+			this.tabControl2.Controls.Add(this.tabPage7);
+			this.tabControl2.Controls.Add(this.tabPage8);
+			this.tabControl2.Location = new System.Drawing.Point(154, 3);
+			this.tabControl2.Name = "tabControl2";
+			this.tabControl2.SelectedIndex = 0;
+			this.tabControl2.Size = new System.Drawing.Size(809, 255);
+			this.tabControl2.TabIndex = 1;
+			// 
+			// tabPage5
+			// 
+			this.tabPage5.Controls.Add(this.listBox3);
+			this.tabPage5.Location = new System.Drawing.Point(4, 22);
+			this.tabPage5.Name = "tabPage5";
+			this.tabPage5.Padding = new System.Windows.Forms.Padding(3);
+			this.tabPage5.Size = new System.Drawing.Size(801, 229);
+			this.tabPage5.TabIndex = 0;
+			this.tabPage5.Text = "시퀸스/기타설정";
+			this.tabPage5.UseVisualStyleBackColor = true;
+			// 
+			// listBox3
+			// 
+			this.listBox3.FormattingEnabled = true;
+			this.listBox3.ItemHeight = 12;
+			this.listBox3.Items.AddRange(new object[] {
+			"스토리",
+			"진보스",
+			"고브소굴",
+			"데쓰매치",
+			"길드사냥터",
+			"우편함",
+			"친구",
+			"명예"});
+			this.listBox3.Location = new System.Drawing.Point(0, 0);
+			this.listBox3.Name = "listBox3";
+			this.listBox3.Size = new System.Drawing.Size(147, 220);
+			this.listBox3.TabIndex = 0;
+			// 
+			// tabPage6
+			// 
+			this.tabPage6.Controls.Add(this.panel3);
+			this.tabPage6.Controls.Add(this.panel4);
+			this.tabPage6.Controls.Add(this.panel2);
+			this.tabPage6.Location = new System.Drawing.Point(4, 22);
+			this.tabPage6.Name = "tabPage6";
+			this.tabPage6.Padding = new System.Windows.Forms.Padding(3);
+			this.tabPage6.Size = new System.Drawing.Size(801, 229);
+			this.tabPage6.TabIndex = 1;
+			this.tabPage6.Text = "스토리";
+			this.tabPage6.UseVisualStyleBackColor = true;
+			// 
+			// tabPage7
+			// 
+			this.tabPage7.Location = new System.Drawing.Point(4, 22);
+			this.tabPage7.Name = "tabPage7";
+			this.tabPage7.Size = new System.Drawing.Size(801, 229);
+			this.tabPage7.TabIndex = 2;
+			this.tabPage7.Text = "진보스/고브소굴";
+			this.tabPage7.UseVisualStyleBackColor = true;
+			// 
+			// tabPage8
+			// 
+			this.tabPage8.Location = new System.Drawing.Point(4, 22);
+			this.tabPage8.Name = "tabPage8";
+			this.tabPage8.Size = new System.Drawing.Size(801, 229);
+			this.tabPage8.TabIndex = 3;
+			this.tabPage8.Text = "길드사냥/데쓰매치";
+			this.tabPage8.UseVisualStyleBackColor = true;
+			// 
+			// panel1
+			// 
+			this.panel1.BackColor = System.Drawing.Color.OrangeRed;
+			this.panel1.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+			this.panel1.Location = new System.Drawing.Point(0, 0);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(150, 259);
+			this.panel1.TabIndex = 0;
 			// 
 			// tabPage3
 			// 
 			this.tabPage3.Location = new System.Drawing.Point(4, 22);
 			this.tabPage3.Name = "tabPage3";
-			this.tabPage3.Size = new System.Drawing.Size(469, 230);
+			this.tabPage3.Size = new System.Drawing.Size(964, 259);
 			this.tabPage3.TabIndex = 2;
 			this.tabPage3.Text = "로 그";
 			this.tabPage3.UseVisualStyleBackColor = true;
@@ -94,22 +335,55 @@ namespace BaKasMacro
 			// 
 			this.tabPage4.Location = new System.Drawing.Point(4, 22);
 			this.tabPage4.Name = "tabPage4";
-			this.tabPage4.Size = new System.Drawing.Size(469, 230);
+			this.tabPage4.Size = new System.Drawing.Size(964, 259);
 			this.tabPage4.TabIndex = 3;
 			this.tabPage4.Text = "기 타";
 			this.tabPage4.UseVisualStyleBackColor = true;
+			// 
+			// panel2
+			// 
+			this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.panel2.Location = new System.Drawing.Point(0, 2);
+			this.panel2.Name = "panel2";
+			this.panel2.Size = new System.Drawing.Size(126, 227);
+			this.panel2.TabIndex = 0;
+			// 
+			// panel3
+			// 
+			this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.panel3.Location = new System.Drawing.Point(531, 11);
+			this.panel3.Name = "panel3";
+			this.panel3.Size = new System.Drawing.Size(126, 227);
+			this.panel3.TabIndex = 1;
+			// 
+			// panel4
+			// 
+			this.panel4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.panel4.Location = new System.Drawing.Point(373, 7);
+			this.panel4.Name = "panel4";
+			this.panel4.Size = new System.Drawing.Size(126, 227);
+			this.panel4.TabIndex = 2;
 			// 
 			// MainForm
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.SystemColors.Control;
 			this.ClientSize = new System.Drawing.Size(984, 362);
 			this.Controls.Add(this.tabControl1);
 			this.Name = "MainForm";
 			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
 			this.Text = "BaKasMacro";
 			this.tabControl1.ResumeLayout(false);
+			this.tabPage1.ResumeLayout(false);
+			this.tabPage1.PerformLayout();
+			this.tabPage2.ResumeLayout(false);
+			this.tabControl2.ResumeLayout(false);
+			this.tabPage5.ResumeLayout(false);
+			this.tabPage6.ResumeLayout(false);
+			this.panel1.ResumeLayout(false);
 			this.ResumeLayout(false);
+
 
 		}
 	}
